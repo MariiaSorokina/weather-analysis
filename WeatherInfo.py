42a3d81cd64ecafa8816e77e31719f997da6b1d8
msorokina <m.sorokina0702@gmail.com>
2023-07-17 15:37:09 +0400

    Add Exception class

--- WeatherInfo.py
+++ WeatherInfo.py
@@ -1,8 +1,13 @@
 import requests
 
+
+class GetWeatherException(Exception):
+    pass
+
+
 class WeatherAPI:
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, api):
+        self.api = api_key
         self.base_url = "http://api.openweathermap.org/data/2.5/find"
 
     def get_weather(self, city_name, country_code):
@@ -10,7 +15,7 @@ class WeatherAPI:
             'q': f'{city_name},{country_code}',
             'type': 'like',
             'units': 'metric',
-            'appid': self.api_key
+            'appid': self.api
         }
 
         response = requests.get(self.base_url, params=params)
@@ -18,24 +23,28 @@ class WeatherAPI:
             data = response.json()
             return data
         else:
-            return None
+            raise GetWeatherException('Failed to retrieve weather data.')
 
     def get_forecast(self, city_name, country_code):
-        weather_data = self.get_weather(city_name, country_code)
+        try:
+            weather_data = self.get_weather(city_name, country_code)
 
-        if weather_data:
-            forecast = {}
-            result = weather_data['list'][0]
+            if weather_data:
+                forecast = {}
+                result = weather_data['list'][0]
 
-            forecast['current_temperature'] = result['main']['temp']
-            forecast['feels_like'] = result['main']['feels_like']
-            forecast['humidity'] = result['main']['humidity']
-            forecast['wind'] = result['wind']['speed']
-            forecast['condition'] = result['weather'][0]['description']
+                forecast['current_temperature'] = result['main']['temp']
+                forecast['feels_like'] = result['main']['feels_like']
+                forecast['humidity'] = result['main']['humidity']
+                forecast['wind'] = result['wind']['speed']
+                forecast['condition'] = result['weather'][0]['description']
 
-            return forecast
-        else:
-            return None
+                return forecast
+            else:
+                raise GetWeatherException("Weather data not found.")
+        except GetWeatherException as e:
+            print(f"An error occurred: {str(e)}")
+
 
 # Пример использования
 file_path = "/Users/msorokina/PycharmProjects/API-ключ OpenWeatherMap"
@@ -45,18 +54,19 @@ with open(file_path, 'r') as file:
 
 weather_api = WeatherAPI(api_key)
 
-city = input('Please enter the city: ').capitalize()
-country = input('Please enter the country: ').capitalize()
-forecast = weather_api.get_forecast(city, country)
+try:
+    city = input('Please enter the city: ').capitalize()
+    country = input('Please enter the country: ').capitalize()
+    forecast = weather_api.get_forecast(city, country)
 
-if forecast:
     print("Weather forecast:")
     print(f"Current temperature: {forecast['current_temperature']}°C")
     print(f"Feels like: {forecast['feels_like']}°C")
     print(f"Humidity: {forecast['humidity']}%")
     print(f"Wind speed: {forecast['wind']} м/с")
-    print(f"Weather condition: {forecast['condition']}")
-else:
-    print("It was not possible to get a weather forecast.")
-
-        # current_temperature, feels_like, humidity, wind, condition
+except KeyError:
+    raise GetWeatherException("Invalid response format.")
+except GetWeatherException as e:
+    print(f"An error occurred: {str(e)}")
+except IndexError:
+    print('Incorrect data entered')
